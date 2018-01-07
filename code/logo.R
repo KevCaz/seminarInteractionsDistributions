b1e6cd693d6f9d6343ccaa5063bfb563f3de1526
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-01-07 14:05:06 -0500

    seminar Grenoble version 1.0.1

--- code/logo.R
+++ code/logo.R
@@ -4,4 +4,4 @@ library("graphicsutils")
 par(mar=c(1,4))
 
 plot0()
-pchImage(0,0, file="./img/Logo_UQAM.png", cex.x=10, col="grey75")
+pchImage(0,0, file="~/Downloads/logoUds.png", cex.x=10, col="#3fb3b2")
